1e55b622d4e7730f0578cf8ea514f68e3e7bb2da
Josh <joshhsoj1902@gmail.com>
2023-10-07 22:43:06 -0400

    Address codacy issues

--- scripts/minecraft-install-plugins.sh
+++ scripts/minecraft-install-plugins.sh
@@ -6,7 +6,7 @@ PluginDir=/data/serverfiles/plugins
 DownloadDir=/app/mc-plugins
 
 function doInstall {
-    if [ $installMode == true ]; then
+    if $installMode; then
         echo "Installing $1"
         cp -p $DownloadDir/$1 $PluginDir
     fi
@@ -21,35 +21,35 @@ if [ -n "$LGSM_MINECRAFT_PLUGIN_DYNMAP_VERSION_ID" ] && [ -n "$LGSM_MINECRAFT_PL
     OutputFile=$LGSM_MINECRAFT_PLUGIN_DYNMAP_VERSION
     if [ ! -f $DownloadDir/$OutputFile ]; then
         echo "Plugin missing, Downloading"
-        curl -o $DownloadDir/$OutputFile https://cdn.modrinth.com/data/fRQREgAc/versions/$LGSM_MINECRAFT_PLUGIN_DYNMAP_VERSION_ID/$LGSM_MINECRAFT_PLUGIN_DYNMAP_VERSION
+        curl -o "$DownloadDir"/"$OutputFile" https://cdn.modrinth.com/data/fRQREgAc/versions/"$LGSM_MINECRAFT_PLUGIN_DYNMAP_VERSION_ID"/"$LGSM_MINECRAFT_PLUGIN_DYNMAP_VERSION"
     fi
 
-    doInstall $OutputFile
+    doInstall "$OutputFile"
 
     echo "=== Done Dynmap"
 fi
 
 if [ -n "$LGSM_MINECRAFT_PLUGIN_ESSENTIALS_VERSION" ]; then
     echo "=== Start EssentialsX ==="
-    OutputFile="EssentialsX-$Version.jar"
+    OutputFile="EssentialsX-$LGSM_MINECRAFT_PLUGIN_ESSENTIALS_VERSION.jar"
     if [ ! -f $DownloadDir/$OutputFile ]; then
         echo "Plugin missing, Downloading"
-        curl -o $DownloadDir/$OutputFile https://github.com/EssentialsX/Essentials/releases/download/$Version/EssentialsX-$Version.jar
+        curl -o "$DownloadDir"/"$OutputFile" https://github.com/EssentialsX/Essentials/releases/download/"$LGSM_MINECRAFT_PLUGIN_ESSENTIALS_VERSION"/EssentialsX-"$LGSM_MINECRAFT_PLUGIN_ESSENTIALS_VERSION".jar
     fi
 
-    doInstall $OutputFile
+    doInstall "$OutputFile"
 
     echo "=== Done EssentialsX"
 fi
 
 if [ -n "$LGSM_MINECRAFT_PLUGIN_UNIFIED_METRICS_VERSION" ]; then
     echo "=== Start Unified metrics ==="
-    OutputFile="unifiedmetrics-platform-bukkit-$Version.jar"
+    OutputFile="unifiedmetrics-platform-bukkit-$LGSM_MINECRAFT_PLUGIN_UNIFIED_METRICS_VERSION.jar"
     if [ ! -f $DownloadDir/$OutputFile ]; then
         echo "Plugin missing, Downloading"
-        curl -o $DownloadDir/$OutputFile https://github.com/Cubxity/UnifiedMetrics/releases/download/$Version/unifiedmetrics-platform-bukkit-$Version.jar
+        curl -o "$DownloadDir"/"$OutputFile" https://github.com/Cubxity/UnifiedMetrics/releases/download/"$LGSM_MINECRAFT_PLUGIN_UNIFIED_METRICS_VERSION"/unifiedmetrics-platform-bukkit-"$LGSM_MINECRAFT_PLUGIN_UNIFIED_METRICS_VERSION".jar
     fi
-    doInstall $OutputFile
+    doInstall "$OutputFile"
 
     echo "=== Done Unified metrics"
 fi
