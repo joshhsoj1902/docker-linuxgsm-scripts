3e925ea19e0aa95e36b3a0021bf0a8eaf39eb425
Josh <joshhsoj1902@gmail.com>
2024-01-27 13:39:44 -0500

    Update entrypoints to latest from https://github.com/GameServerManagers/docker-linuxgsm

--- entrypoints/entrypoint-user.sh
+++ entrypoints/entrypoint-user.sh
@@ -30,6 +30,15 @@ if [ ! -f "${GAMESERVER}" ]; then
   ./linuxgsm.sh "${GAMESERVER}"
 fi
 
+# Symlink LGSM_CONFIG to /app/lgsm/config-lgsm
+if [ ! -d "/app/lgsm/config-lgsm" ]; then
+  echo -e ""
+  echo -e "creating symlink for ${LGSM_CONFIG}"
+  echo -e "================================="
+  ln -s "${LGSM_CONFIG}" "/app/lgsm/config-lgsm"
+fi
+
+
 # Clear modules directory if not master
 if [ "${LGSM_GITHUBBRANCH}" != "master" ]; then
   echo -e "not master branch, clearing modules directory"
@@ -39,6 +48,7 @@ elif [ -d "/app/lgsm/modules" ]; then
   echo -e "ensure all modules are executable"
   chmod +x /app/lgsm/modules/*
 fi
+
 
 # If a command was passed in. Run it and exit
 if [ "$1" != "" ]; then
@@ -55,6 +65,12 @@ if [ -d "/app/hooks/pre-install" ]; then
   execute_hook_directory "/app/hooks/pre-install/*.sh"
 fi
 
+# Enable developer mode
+if [ "${LGSM_DEV}" == "true" ]; then
+  echo -e "developer mode enabled"
+  ./"${GAMESERVER}" developer
+fi
+
 # Install game server
 if [ -z "$(ls -A -- "/data/serverfiles" 2> /dev/null)" ]; then
   echo -e ""
@@ -67,12 +83,12 @@ else
   # Sponsor to display LinuxGSM logo
   ./"${GAMESERVER}" sponsor
 fi
+
 echo -e ""
 echo -e "Starting Update Checks"
 echo -e "================================="
-nohup watch -n "${UPDATE_CHECK}" ./"${GAMESERVER}" update > /dev/null 2>&1 &
-minutes=$((UPDATE_CHECK / 60))
-echo -e "update will check every ${minutes} minutes"
+echo -e "*/${UPDATE_CHECK} * * * * /app/${GAMESERVER} update > /dev/null 2>&1" | crontab -
+echo -e "update will check every ${UPDATE_CHECK} minutes"
 
 # Update game server
 if [ -z "${install}" ]; then
@@ -82,7 +98,7 @@ if [ -z "${install}" ]; then
   ./"${GAMESERVER}" update
 fi
 
-# Run pre-install scripts
+# Run post-install scripts
 if [ -d "/app/hooks/post-install" ]; then
   echo -e ""
   echo -e "Executing post-install hooks"
@@ -99,5 +115,5 @@ sleep 5
 sleep 2
 echo -e "Tail log files"
 echo -e "================================="
-tail -F "${LGSM_LOGDIR}"/*/*.log &
+tail -F "${LGSM_LOGDIR}"/{console,script}/*{console,script}.log &
 wait
